2ac4243ca6588ad8d484f73cf66e954104beee6b
hexadoo <hexadoo@hotmail.com>
2023-01-12 21:28:16 -0500

    Update antminer.py

--- antminer.py
+++ antminer.py
@@ -1,22 +1,15 @@
+
+import datetime
+import git
 import time
-from datetime import datetime
-import os
 
-current_path = os.getcwd()
-
+repo = git.Repo('https://github.com/hexadoo/dockerpython.git')
 
 while True:
-    # Get the current date and time
-    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    # # Append the current date and time and the word "test" to the file
-    # with open("/local_output/hello_world.txt", "a") as file:
-    #     file.write(f"{now} test\n")
-        
-    with open(current_path + "\hello_world.txt", "a") as file:
-        file.write(f"{now} test\n")
-    
-    print(now)
-    print(current_path)
-    # Sleep for 1 minute
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    with open('time.txt', 'a') as f:
+        f.write(current_time + '\n')
+    repo.git.add('hello_world.txt')
+    repo.git.commit(m='Added current time')
+    repo.git.push()
     time.sleep(60)
